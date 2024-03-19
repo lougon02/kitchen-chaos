476aa9e8e8e3309dfd4a247252f08ef0d4e406e0
Lourenço Gonçalves <lourencogon02@gmail.com>
2024-03-19 18:58:14 +0000

    added cutting recipes

--- Assets/Scripts/CuttingCounter.cs
+++ Assets/Scripts/CuttingCounter.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class CuttingCounter : BaseCounter {
 
-    [SerializeField] private KitchenObjectSO cutKitchenObjectSO;
+    [SerializeField] private CuttingRecipeSO[] cuttingRecipeSOArray;
     public override void Interact(Player player) {
         if (!HasKitchenObject()) {
             if (player.HasKitchenObject()) {
-                player.GetKitchenObject().SetKitchenObjectParent(this);
+                if (HasRecipe(player.GetKitchenObject().GetKitchenObjectSO())) {
+                    player.GetKitchenObject().SetKitchenObjectParent(this);
+                }
             }
         } else {
             if (!player.HasKitchenObject())
@@ -18,9 +20,26 @@ public class CuttingCounter : BaseCounter {
 
     public override void InteractAlternate(Player player) {
         if (HasKitchenObject()) {
-            GetKitchenObject().DestroySelf();
+            KitchenObjectSO cutKitchenObjectSO = getOutputFromInput(GetKitchenObject().GetKitchenObjectSO());
 
+            if (cutKitchenObjectSO == null) return;
+
+            GetKitchenObject().DestroySelf();
             KitchenObject.SpawnKitchenObject(cutKitchenObjectSO, this);
         }
+    }
+
+    private bool HasRecipe(KitchenObjectSO input) {
+        return getOutputFromInput(input) != null;
+    }
+
+    private KitchenObjectSO getOutputFromInput(KitchenObjectSO input) {
+        foreach (CuttingRecipeSO cuttingRecipeSO in cuttingRecipeSOArray) {
+            if (cuttingRecipeSO.input == input) {
+                return cuttingRecipeSO.output;
+            }
+        }
+
+        return null;
     }
 }
