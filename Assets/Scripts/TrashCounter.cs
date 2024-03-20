74e35dab7007704c025f9891f1c56d47053b83fb
Lourenço Gonçalves <lourencogon02@gmail.com>
2024-03-20 13:35:54 +0000

    lookatcamera script and trash counter

---
+++ Assets/Scripts/TrashCounter.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashCounter : BaseCounter {
+
+    public override void Interact(Player player) {
+        player.GetKitchenObject().DestroySelf();
+    }
+}
